2accdf2ee0c4932dcb2cb2eb0ab06c519cdfa54e
userthalib <thalibirfan29@gmail.com>
2022-03-23 18:50:11 +0700

    membuat Nesting router

--- tugas5/route-react/src/App.js
+++ tugas5/route-react/src/App.js
@@ -1,66 +1,118 @@
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+// } from "react-router-dom"
+
+// export default function BasicExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/dashboard">Dashboard</Link>
+//           </li>
+//         </ul>
+//         <hr />
+//         <Switch>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+//           <Route exact path="/about">
+//             <About />
+//           </Route>
+//           <Route exact path="/dashboard">
+//             <Dashboard />
+//           </Route>
+//         </Switch>
+//     </div>
+//     </Router>
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//     </div>
+//   );
+// }
+
+// function About() {
+//   return (
+//     <div>
+//       <h2>About</h2>
+//     </div>
+//   );
+// }
+
+// function Dashboard() {
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//     </div>
+//   );
+// }
+
 import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useParams
 } from "react-router-dom"
 
-export default function BasicExample() {
+export default function ParamsExample() {
   return (
     <Router>
       <div>
+        <h2>Accounts</h2>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/netflix">Netflix</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/gmail">Gmail</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/amazon">Amazon</Link>
           </li>
         </ul>
-        <hr />
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
+          <Route path="/:id" children={<Child />} />
         </Switch>
     </div>
     </Router>
   );
 }
 
-function Home() {
+function Child() {
+  let { id } = useParams();
+
   return (
     <div>
-      <h2>Home</h2>
+      <h3>ID: {id}</h3>
     </div>
   );
 }
 
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
 
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
+
+
+
+
 
 
 
