cc5b4982585e99efd247503baf0370e0e3d6da14
userthalib <thalibirfan29@gmail.com>
2022-03-24 19:25:07 +0700

    update CreateTodo

--- tugas6/react-redux/src/Containers/CreateTodo.js
+++ tugas6/react-redux/src/Containers/CreateTodo.js
@@ -22,7 +22,7 @@ class CreateTodo extends Component {
  <input onChange={this.onChangeTodoText} value={this.state.todotext} 
 type="text" className="form-control" id="inputEmail3" placeholder="add todo here"/>
  <button type="button" onClick={ () => this.setState({ todotext: '' }) } 
-style={{marginTop: "25px", marginRight: "15px"}} className="btn btndanger">Cancel</button>
+style={{marginTop: "25px", marginRight: "15px"}} className="btn btn-danger">Cancel</button>
  <button type="button" onClick={() =>{ 
 this.props.addTodo(this.state.todotext); this.setState({ todotext: '' }) } } 
 style={{marginTop: "25px"}} className="btn btn-success">Add Todo</button>
