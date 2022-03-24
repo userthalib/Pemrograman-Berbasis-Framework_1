4d7b8a2edf6dfe904d26d2f702546eeac01b7429
userthalib <thalibirfan29@gmail.com>
2022-03-24 19:34:33 +0700

    mengedit class index.js

--- tugas6/react-redux/src/index.js
+++ tugas6/react-redux/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
 
+const store = compose(
+  window.devToolsExtension ? window.devToolsExtension(
+  ) : f => f)(createStore)(MainReducer)
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
