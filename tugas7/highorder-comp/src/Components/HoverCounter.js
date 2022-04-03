2dd88387de8af755cc74b0fd9d44e79afc33295f
userthalib <thalibirfan29@gmail.com>
2022-04-03 13:51:26 +0700

    menambah komponen untuk counter sendiri

--- tugas7/highorder-comp/src/Components/HoverCounter.js
+++ tugas7/highorder-comp/src/Components/HoverCounter.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import UpdatedComponent from './withCounter';
+import withCounter from './withCounter';
 
 class HoverCounter extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            count: 0
-        }
-    }
-    incrementCount = () => {
-        this.setState(prevState => {
-            return { count: prevState.count + 1 }
-        })
-    }
+    
     render() {
-        const { count } = this.state
+        const { count, incrementCount } = this.props
         return (
-            <h2 onMouseOver={this.incrementCount}>
-                {this.props.name} Hovered {count} Times</h2>
+            <h2 onMouseOver={incrementCount}>
+                Hovered {count} Times</h2>
         );
     }
 }
 
-export default UpdatedComponent(HoverCounter);
+export default withCounter(HoverCounter);
