af2115a21fc1bcddecdb27c6215f3b73ca960dbc
userthalib <thalibirfan29@gmail.com>
2022-04-25 02:13:26 +0700

    update index

--- tugas10/firebase-js/src/index.js
+++ tugas10/firebase-js/src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import routes from "./routes";
+import Header from "./Header";
+import "./style.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const AuthContext = React.createContext(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      is LoggedIn in? {JSON.strinify(isLoggedIn)}
+      <div className="App">
+        <Router>
+          <Header />
+          <Switch>
+            {routes.map(route => (
+              <Route
+                key={route.path}
+                path={route.path}
+                exact={route.exact}
+                component={route.main}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App/>, rootElement);
