07634b346f76805f442a2a5660c9ad38a59ff631
userthalib <thalibirfan29@gmail.com>
2022-04-26 15:32:54 +0700

    update firebaseconfig

---
+++ tugas10/firebase-js/src/firebase-config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+const firebaseConfig = {
+  apiKey: "AIzaSyA0I1l1QXmo0-xRhHFqdnq-E7esFDq9yfA",
+  authDomain: "fir-js-c6a36.firebaseapp.com",
+  projectId: "fir-js-c6a36",
+  storageBucket: "fir-js-c6a36.appspot.com",
+  messagingSenderId: "560975821434",
+  appId: "1:560975821434:web:7ed8e27f4c31c2694599c3",
+  measurementId: "G-XZQG0Q6P4T"
+};
