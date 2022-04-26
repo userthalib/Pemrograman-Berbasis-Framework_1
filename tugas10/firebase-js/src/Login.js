68504cb0f0bca8eeef8d35eabb58fa2cc39bd1e0
userthalib <thalibirfan29@gmail.com>
2022-04-26 15:40:08 +0700

    update login

--- tugas10/firebase-js/src/Login.js
+++ tugas10/firebase-js/src/Login.js
@@ -1,49 +1,64 @@
-import React, { useState, useContext } from 'react'
-import { AuthContext } from './index';
+import React, { useContext, useState } from 'react'
+import { AuthContext } from './index'
+import firebase from 'firebase'
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setErrors] = useState("");
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setErrors] = useState('')
 
-    const Auth = useContext(AuthContext);
-    const handleForm = e => {
-        e.preventDefault();
-        console.log(Auth);
-        Auth.setLoggedIn(true);
-    };
+  const Auth = useContext(AuthContext)
+  const handleForm = e => {
+    e.preventDefault()
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(res => {
+        if (res.user) Auth.setLoggedIn(true)
+      })
+      .catch(e => {
+        setErrors(e.message)
+      })
+  }
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={e => handleForm(e)}>
-                <input
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    name="email"
-                    type="email"
-                    placeholder="email"
-                />
-                <input
-                    onChange={e => setEmail(e.target.value)}
-                    name="password"
-                    value={password}
-                    type="password"
-                    placeholder="password"
-                />
-                <hr />
-                <button class="googleBtn" type="button">
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                        alt="logo"
-                    />
-                    Login With Google
-                </button>
-                <button type="submit">Login</button>
-                <span>{error}</span>
-            </form>
-        </div>
-    );
-};
+  const handleButtonLogin = () => {
+    firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(res => {
+        if (res.user) Auth.setLoggedIn(true)
+      })
+      .catch(e => {
+        setErrors(e.message)
+      })
+  }
 
-export default Login;
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={e => handleForm(e)}>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          name='email'
+          type='email'
+          placeholder='email' />
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          name='password'
+          type='password'
+          placeholder='password' />
+        <hr />
+        <button className='googleBtn' type='button' onClick={handleButtonLogin}>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg' alt='logo' />
+          Login with Google
+        </button>
+        <button type='submit'>Login</button>
+        <span>{error}</span>
+      </form>
+    </div>
+  )
+}
+
+export default Login
